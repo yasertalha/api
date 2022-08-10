10b3fc8480920f7d46e4f998418a34da2a635c6d
MSyed.YaserMohasin <MSyed.YaserMohasin@lntinfotech.com>
2022-08-10 20:11:49 +0530

    display loader on api fetch

--- mycustomapi_frontEnd/src/Redux/actions/action.js
+++ mycustomapi_frontEnd/src/Redux/actions/action.js
@@ -2,9 +2,11 @@ import {postHttp} from '../../Utils/CommonHttp'
 
 export function createApi(body) {
     return async (dispatch) => {
+        dispatch({ type: "UPDATE_LOADING_STATUS", payload: {status:true} })
         const secretID = await postHttp("/create",body)
        if(secretID){
             dispatch({ type: "SUCCESS_CREATE", payload: {secretID,preInput:body} })
        }
+       dispatch({ type: "UPDATE_LOADING_STATUS", payload: {status:false} })
     }
 }
